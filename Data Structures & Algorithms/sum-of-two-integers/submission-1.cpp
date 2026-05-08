3aa254f565731e65c170a4bff273c97b94296a43
Krishna Swaroop <55535944+krishroop@users.noreply.github.com>
2026-05-08 18:41:43 +0530

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+    while (b != 0) {
+        int carry = (int)(a & b) << 1;
+
+        a = a ^ b;   // sum without carry
+        b = carry;   // carry
+    }
+
+    return a;
+}
+};
