691bcedc1379c65ba671d5665dd97341efc3bee2
Krishna Swaroop <55535944+krishroop@users.noreply.github.com>
2026-05-10 16:30:32 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+
+        string res;
+
+        for (string &s : strs) {
+
+            res += to_string(s.size()) + '#' + s;
+        }
+
+        return res;
+    }
+
+    vector<string> decode(string s) {
+
+    vector<string> ans;
+
+    int i = 0;
+
+    while (i < s.size()) {
+
+        int j = i;
+
+        while (j < s.size() && s[j] != '#')
+            j++;
+
+        int len = stoi(s.substr(i, j - i));
+
+        string word = s.substr(j + 1, len);
+
+        ans.push_back(word);
+
+        i = j + 1 + len;
+    }
+
+    return ans;
+}
+};
