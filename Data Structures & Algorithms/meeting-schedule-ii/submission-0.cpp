ed7a9d2392ade20821ec32c7721a840fd5bc4954
Krishna Swaroop <55535944+krishroop@users.noreply.github.com>
2026-05-12 21:03:30 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+
+    int minMeetingRooms(vector<Interval>& intervals) {
+
+        if (intervals.empty())
+            return 0;
+
+        sort(intervals.begin(), intervals.end(),
+            [](Interval &a, Interval &b) {
+                return a.start < b.start;
+            });
+
+        priority_queue<int,
+                       vector<int>,
+                       greater<int>> pq;
+
+        // first meeting
+        pq.push(intervals[0].end);
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            // room becomes free
+            if (intervals[i].start >= pq.top()) {
+                pq.pop();
+            }
+
+            // occupy room
+            pq.push(intervals[i].end);
+        }
+
+        return pq.size();
+    }
+};
